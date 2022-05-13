99fa21af5554a58c98bed61b90e38ad9de07011f
Sean Baier <sean.t.baier@gmail.com>
2022-05-13 03:02:42 -0500

    behavioral patterns

---
+++ behavioral_patterns/interpreter/boiler.py
@@ -0,0 +1,47 @@
+"""
+event ::= command token receiver token arguments
+command ::= word+
+word ::= a collection of one or more alphamnueric characters
+token ::= ->
+receiver ::= word+
+arguments ::= word+
+"""
+from pyparsing import Group, OneOrMore, Optional, Suppress, Word, alphanums
+
+
+class Boiler:
+    def __init__(self):
+        self.temperature = 83
+
+    def __str__(self):
+        return f"boiler temperature: {self.temperature}"
+
+    def increase_temperature(self, amount):
+        print(f"increasing the boiler's termperature by {amount} degrees")
+        self.temperature += amount
+
+    def decrease_temperature(self, amount):
+        print(f"decreasing the boiler's temperature by {amount} degrees")
+        self.temperature -= amount
+
+
+word = Word(alphanums)
+command = Group(OneOrMore(word))
+token = Suppress("->")
+device = Group(OneOrMore(word))
+argument = Group(OneOrMore(word))
+
+event = command + token + device + Optional(token + argument)
+
+boiler = Boiler()
+print(boiler)
+
+cmd, dev, arg = event.parseString("increase -> boiler temperature -> 3 degrees")
+cmd_str = " ".join(cmd)
+dev_str = " ".join(dev)
+
+
+if "increase" in cmd_str and "boiler" in dev_str:
+    boiler.increase_temperature(int(arg[0]))
+
+print(boiler)
